bfc5c6ab67fbd4a46288706f3564dd610126eaa8
Patrick McCaffrey <patrick.mccaffrey.19@gmail.com>
2016-10-27 22:10:18 +0000

    time plus newline

--- time.js
+++ time.js
@@ -8,12 +8,12 @@
 var net = require("net")
 var strftime = require("strftime")
 
-var port = process.argv[2]
+var port = Number(process.argv[2])
     
 var server = net.createServer( (socket) => {
     
     // create the date object
-    var data = strftime('%F %R')
+    var data = strftime('%F %R%n')
     
     socket.write(data)
     socket.end()    
@@ -21,3 +21,26 @@ var server = net.createServer( (socket) => {
 
 
 server.listen(port)
+
+// OFFICIAL SOLUTION
+
+var net = require('net')  
+       
+     function zeroFill (i) {  
+       return (i < 10 ? '0' : '') + i  
+     }  
+       
+     function now () {  
+       var d = new Date()  
+       return d.getFullYear() + '-' +  
+         zeroFill(d.getMonth() + 1) + '-' +  
+         zeroFill(d.getDate()) + ' ' +  
+         zeroFill(d.getHours()) + ':' +  
+         zeroFill(d.getMinutes())  
+     }  
+       
+     var server = net.createServer(function (socket) {  
+       socket.end(now() + '\n')  
+     })  
+       
+     server.listen(Number(process.argv[2]))
